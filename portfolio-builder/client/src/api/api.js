61c02a44188f46f17c31d4e9761381af00d42a2f
varunchandra10 <kola.varunchandra29@gmail.com>
2025-02-09 20:09:11 +0530

    server_connection_updated

--- portfolio-builder/client/src/api/api.js
+++ portfolio-builder/client/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000/" });
-// const API = axios.create({ baseURL: "https://portfolio-builder-cfbp.onrender.com" });
+// const API = axios.create({ baseURL: "http://localhost:5000/" });
+const API = axios.create({ baseURL: "https://portfoliobuilder-server.onrender.com" });
 
 
 API.interceptors.request.use((req) => {
